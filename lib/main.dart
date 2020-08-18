a5236cf504748276b70b1553ef5c32f495f4bad7
Marco Dijkslag <m.dijkslag@student.utwente.nl>
2020-08-18 18:50:44 +0200

    ensure flutter bindings initialized

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ import 'ui/button_round.dart';
 import 'ui/dpad_arrow.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,28 +62,47 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                DpadArrow(Icons.arrow_back, "D_LEFT", false, buttonDown, buttonUp),
-                Padding(padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),),
-                DpadArrow(Icons.arrow_forward, "D_RIGHT", false, buttonDown, buttonUp),
+                DpadArrow(
+                    Icons.arrow_back, "D_LEFT", false, buttonDown, buttonUp),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
+                ),
+                DpadArrow(Icons.arrow_forward, "D_RIGHT", false, buttonDown,
+                    buttonUp),
               ],
             ),
-            DpadArrow(Icons.arrow_downward, "D_DOWN", true, buttonDown, buttonUp),
-            SizedBox(height: 50.0,),
+            DpadArrow(
+                Icons.arrow_downward, "D_DOWN", true, buttonDown, buttonUp),
+            SizedBox(
+              height: 50.0,
+            ),
             PadRoundButton(null, "A", 65.0, 65.0, buttonDown, buttonUp),
-            SizedBox(height: 15.0,),
+            SizedBox(
+              height: 15.0,
+            ),
             PadRectangleButton(null, "B", 65.0, 85.0, buttonDown, buttonUp),
-            SizedBox(height: 50.0,),
+            SizedBox(
+              height: 50.0,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                PadRoundButton(Icons.remove, "MINUS", 30.0, 30.0, buttonDown, buttonUp),
-                PadRoundButton(Icons.home, "HOME", 30.0, 30.0, buttonDown, buttonUp),
-                PadRoundButton(Icons.add, "PLUS", 30.0, 30.0, buttonDown, buttonUp),
+                PadRoundButton(
+                    Icons.remove, "MINUS", 30.0, 30.0, buttonDown, buttonUp),
+                PadRoundButton(
+                    Icons.home, "HOME", 30.0, 30.0, buttonDown, buttonUp),
+                PadRoundButton(
+                    Icons.add, "PLUS", 30.0, 30.0, buttonDown, buttonUp),
               ],
             ),
-            SizedBox(height: 50.0,),
+            SizedBox(
+              height: 50.0,
+            ),
             PadRoundButton(null, "1", 45.0, 45.0, buttonDown, buttonUp),
-            SizedBox(height: 15.0,),
+            SizedBox(
+              height: 15.0,
+            ),
             PadRoundButton(null, "2", 45.0, 45.0, buttonDown, buttonUp),
           ],
         ),
